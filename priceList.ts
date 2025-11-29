b35361ca32c4dc547f6430878c73d611b7586c73
MitchJohn <mitchell@kiwisprout.nz>
2025-11-30 10:23:47 +1300

    z

--- priceList.ts
+++ priceList.ts
@@ -8,15 +8,27 @@ export const PriceRuleResult = z.object({
     z.literal("PRODUCT"),
     z.literal("VARIANT"),
     z.literal("COLLECTION"),
-  ]).describe("The type of object this price rule applies to"),
+  ]).nullable().optional().describe("The type of object this price rule applies to"),
   contextId: z.string().nullable().optional().describe("The applying collection/product id (empty for store)"),
   contextTitle: z.string().nullable().optional().describe("The contextual title"),
   contextImage: z.string().nullable().optional().describe("The contextual image"),
   discountMethod: z.union([
-    z.literal("SET_PRICE"),
-    z.literal("PERCENTAGE"),
-  ]).describe("The type of price/discount"),
-  discountAmount: z.number().nullable().optional().describe("The set price value or percentage discount (e.g. 0.3 for 30%)")
+    z.literal("SET_PRICE").describe("A new price or certain price"),
+    z.literal("PERCENTAGE").describe("A percentage off the original price"),
+    z.literal("FIXED").describe("A fixed amount off the original price"),
+  ]).nullable().optional().describe("The type of price/discount"),
+  discountAmount: z.number().nullable().optional().describe("The set price value or percentage discount (e.g. 0.3 for 30%)"),
+  requiredMinSubtotal: z.number().nullable().optional().describe("The required minimum subtotal amount to activate this discount rule"),
+  requiredMinQuantity: z.number().nullable().optional().describe("The required minimum quantity amount to activate this discount rule"),
+  requiredMaxSubtotal: z.number().nullable().optional().describe("The required maximum subtotal amount to activate this discount rule"),
+  requiredMaxQuantity: z.number().nullable().optional().describe("The required maximum quantity amount to activate this discount rule"),
+  requiredConditionsMethod: z.union([
+    z.literal("SAME_VARIANT").describe("Requires same variant to meet required subtotal/quantity condition"),
+    z.literal("SAME_PRODUCT").describe("Requires same product (any variant) to meet required subtotal/quantity condition"),
+    z.literal("SAME_GROUP").describe("Requires products in this discount group (collection, products, etc) to meet  required subtotal/quantity condition"),
+    z.literal("SAME_RULE").describe("Requires products in this discount rule (collection, products, etc) to meet  required subtotal/quantity condition"),
+    z.literal("ANY").describe("Any product/variant"),
+  ]).describe("Describes how the required subtotal/quantity conditions will be determined"),
 });
 
 export type PriceRule = z.infer<typeof PriceRuleResult>;
@@ -27,6 +39,18 @@ export const PriceListInputResult = z.object({
   priceRules: z.array(PriceRuleResult).nullable().optional(),
   priority: z.number().nullable().optional().describe("The highest priority number is picked first if multiple discounts applicable"),
   enabled: z.boolean().nullable().optional().describe("Whether this price list is enabled or not"),
+  markets: z.array(z.string().nullable().optional()).nullable().optional().describe("The enabled markets for this price list, if empty than all are enabled"),
+  startDate: z.date().nullable().optional().describe("When this pricelist goes into effect"),
+  endDate: z.date().nullable().optional().describe("When this pricelist is no longer in effect"),
+  ruleSelectionMethod: z.union([
+    z.literal("HIGHEST_DISCOUNT").describe("Pick the rule that will result in the highest discount"),
+    z.literal("LOWEST_DISCOUNT").describe("Pick the rule that will result in the lowest discount"),
+    z.literal("MOST_SPECIFIC").describe("Pick the rule that is the most specific e.g. Variant => product => collection => store"),
+  ]).nullable().optional().describe("Describes what rule will be selected when multiple conflicting rules"),
+  calculationMethod: z.union([
+    z.literal("REGULAR_PRICE").describe("Use the regular price"),
+    z.literal("HIGHEST_PRICE").describe("Use the highest price between regular and compare price"),
+  ]).nullable().optional().describe("Describes how percentage or fixed off will be calculated"),
 });
 
 export type PriceListInput = z.infer<typeof PriceListInputResult>;
